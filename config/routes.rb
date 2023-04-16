fabc7f5e5df3429b027c5de2d6f129cb11523bd5
jjavier <razornice@gmail.com>
2023-04-16 17:10:57 -0400

    bug destroy_user_session fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:new, :create, :index]
+  resources :posts, only: [:new, :create]
   devise_for :users
+  devise_scope :user do
+    get 'sign_out', to: 'devise/sessions#destroy'
+    get 'sign_in', to: 'devise/sessions#new'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
